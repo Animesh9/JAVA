c18257919c298b9dc95d939fb79d2f23b26bb928
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-22 12:09:00 +0530

    Create Biggest Num.java

---
+++ Biggest Num.java
@@ -0,0 +1,11 @@
+public class BiggestNum {
+public static void main(String args[]) {
+int num1 = 15 , num2 = 25 , num3 = 41;
+if (num1 >= num2 && num1 >= num3)
+System.out.println( num1 + " is the maximum number.");
+else if (num2 >= num1 && num2 >= num3)
+System.out.println( num2 + " is the maximum number.");
+else
+System.out.println( num3 + " is the maximum number.");
+}
+}
