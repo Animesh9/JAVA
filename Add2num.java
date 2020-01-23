8874aefd83f2d5586073cdd314d24f46388f7e25
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-23 16:25:33 +0530

    Update Add2num.java
    Using command line arguments

--- Add2num.java
+++ Add2num.java
@@ -1,5 +1,9 @@
 public class Add2Num{
 public static void main (String[] args)
 {
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+int c=a+b;
+System.out.println("Add is:"+c);
 }
 }
