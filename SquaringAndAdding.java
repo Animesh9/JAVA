6074d4a97a942a517064b7ffc86f05f093435963
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-27 15:37:00 +0530

    Update SquaringAndAdding.java

--- SquaringAndAdding.java
+++ SquaringAndAdding.java
@@ -1,1 +1,26 @@
 import java.util.Scanner;
+public class Squaring
+{
+public static void main(String args[])
+{
+Scanner s = new Scanner(System.in);
+int a[] = new int[11];
+int x[] = new int[11];
+int t=0;
+System.out.println("Enter 10 values ");
+for (int i=1 ; i<=10 ; i++)
+{
+System.out.println("Enter " + i + "term ");
+a[i]= s.nextInt();
+
+}
+System.out.println("Square of the Numbers");
+for (int k=0 ; k<10 ; k++)
+{
+x[k]= a[k]*a[k];
+System.out.println(x[k]);
+t=t+x[k];
+}
+System.out.println("Sum is: " + t);
+}
+}
