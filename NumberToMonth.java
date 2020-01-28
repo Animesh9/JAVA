e0726178bbd1d0f397e13f19a2d2633df0ef213e
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-28 14:53:42 +0530

    Create NumberToMonth.java

---
+++ NumberToMonth.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Months{
+public static void main(String args[])
+{
+
+Scanner s = new Scanner(System.in);
+
+
+System.out.println("Enter the number t find the month " );
+int n = s.nextInt();
+System.out.println("The month is "+ n);
+
+
+}
+}
