6252308df271bda481adefb8e49e7eddf85fc26a
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-28 14:54:58 +0530

    Update NumberToMonth.java

--- NumberToMonth.java
+++ NumberToMonth.java
@@ -2,14 +2,35 @@ import java.util.Scanner;
 public class NumberToMonth{
 public static void main(String args[])
 {
-
 Scanner s = new Scanner(System.in);
-
-
 System.out.println("Enter the number t find the month " );
 int n = s.nextInt();
 System.out.println("The month is "+ n);
-
-
+if (n==1)
+System.out.println("January");
+else if(n==2)
+System.out.println("February");
+else if(n==3)
+System.out.println("March");
+else if(n==4)
+System.out.println("April");
+else if(n==5)
+System.out.println("May");
+else if(n==6)
+System.out.println("June");
+else if(n==7)
+System.out.println("July");
+else if(n==8)
+System.out.println("August");
+else if(n==9)
+System.out.println("September");
+else if(n==10)
+System.out.println("October");
+else if(n==11)
+System.out.println("November");
+else if(n==12)
+System.out.println("December");
+else 
+System.out.println("There are only 12 months :)");
 }
 }
