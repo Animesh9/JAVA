f1df68343ee6b15d0c393a0acfbfb89d226c3e6f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-09 01:01:31 +0530

    Update PrivateClassAccess.java

--- PrivateClassAccess.java
+++ PrivateClassAccess.java
@@ -6,3 +6,16 @@ room(int x,int y)
 int area()
 {return(l*b);}
 }
+class class_room extends room
+{
+int h;
+class_room(int x,int y,int z)
+{
+super(x,y);
+h=z;
+}
+int volume()
+{
+return(area()*h);
+}
+}
