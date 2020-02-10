fbd5fe441f255fe86e558a9fccd89a40df381895
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-10 10:33:40 +0530

    Create player.java

---
+++ player.java
@@ -0,0 +1,13 @@
+class player
+{
+String name;
+int age;
+player(String n, int a)
+{name=n; age=a;}
+void show()
+{
+System.out.println("\n");
+System.out.println("Player name : "+name);
+System.out.println("Age : "+age);
+}
+}
