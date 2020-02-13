f986369c30a73ef153764f6842b4abd7e97811f7
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-13 16:22:19 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,6 +3,12 @@ public class SelectionSort
 {
 public static void main (String args[])
 {
+int n, k;
+int t=0;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter the Number of elements in array");
+n = s.nextInt();
+int [] i = new int[n];
 
 }
 }
