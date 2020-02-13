efb1b598ac06ccf074682380e959bbee1aa012bf
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-13 16:24:21 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -9,6 +9,22 @@ Scanner s = new Scanner(System.in);
 System.out.println("Enter the Number of elements in array");
 n = s.nextInt();
 int [] i = new int[n];
+for (k=0;k<n;k++)
+{
+i[k]=s.nextInt();
+}
+for (k=0;k<n-1;k++)
+{
+for (int j = k + 1 ; j < n ; j++)
+{
+if(i[k]>i[j])
+{
+t=i[k];
+i[k]=i[j];
+i[j]=t;
+}
+}
+}
 
 }
 }
