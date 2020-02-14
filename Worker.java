5b8861d87609d31c903963c9dfee61acac3425da
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-14 11:20:48 +0530

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -1,1 +1,14 @@
-
+class worker
+{
+String n;
+int no;
+worker(String n, int no){
+this.n=n;
+this.no=no;
+}
+void show()
+{
+System.out.println("Employee Number :-"+ no );
+System.out.println("Employee Name :-"+ n );
+}
+}
