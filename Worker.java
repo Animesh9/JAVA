cb76148e2e4e01f2bf5a9e7ea77f61494d9f484b
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-14 11:25:20 +0530

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -6,4 +6,21 @@ worker(String n, int no){
 this.n=n;
 this.no=no;
 }
-
+void show()
+{
+System.out.println("Employee Number :-"+ no );
+System.out.println("Employee Name :-"+ n );
+}
+}
+class dailyworker extends worker
+{
+int r ;
+dailyworker(String n, int no, int r){
+super(n, no);
+this.r=r;}
+void company (int h)
+{
+show();
+System.out.println("Salary :-"+r*h );
+}
+}
