22d75bae4b40288dad26104837f85fe4f8b53a00
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-26 11:12:23 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class Prime{
+public static void main(String args[])
+{
+    int n, i, prime = 0;
+    System.out.println("Enter a positive integer: ");
+    Scanner s = new Scanner();
+    
+}
+}
