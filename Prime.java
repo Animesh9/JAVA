b861f0ca23033da400a2282f5c2fcaf8f7edff69
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-26 11:17:08 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -5,7 +5,25 @@ public static void main(String args[])
     int n, i, prime = 0;
     System.out.println("Enter a positive integer: ");
     Scanner s = new Scanner();
-    
-    
+    n = s.nextInt();
+    for (i = 2; i <= n / 2; ++i) 
+    {
+        if (n % i == 0) 
+        {
+            prime = 1;
+            break;
+        }
+    }
+    if (n == 1) 
+    {
+        System.out.println("1 is neither prime nor composite.");
+    }
+    else 
+    {
+        if (prime == 0)
+            System.out.println(n + "is a prime number.");
+        else
+            System.out.println(n + " is not a prime number.");
+    }
 }
 }
