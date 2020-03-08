a70ff3e0df30b5c31cb1294544957839691b102f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-09 00:04:38 +0530

    abstract class animals

--- Zoo.java
+++ Zoo.java
@@ -1,1 +1,26 @@
 import java.util.Scanner;
+
+abstract class Animals
+{
+String name; 
+int age;
+String type;
+Animals(String name , int age , String type)
+{
+this.name = name;
+this.age = age;
+this.type  = type;
+}
+String getName(String name)
+{
+return name;
+}
+int getAge(int age)
+{
+return age;
+}
+void talk()
+{
+System.out.println("Name " + name + "\nAge " + age + "\nType " + type);
+}
+}
