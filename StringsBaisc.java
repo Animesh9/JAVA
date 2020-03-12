1d4916bc4692592a7430366cc691c7d21abb078c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-12 11:58:59 +0530

    main

--- StringsBaisc.java
+++ StringsBaisc.java
@@ -7,5 +7,15 @@ String A=sc.next();
 String B=sc.next();
 System.out.println(A.length()+B.length());
 int i = A.compareTo(B);
-
+if(i>0){
+System.out.println("Yes");
+} 
+else
+{
+System.out.println("No");
+}
+String atemp = A.substring(0,1).toUpperCase()+A.substring(1);
+String btemp = B.substring(0,1).toUpperCase()+B.substring(1);;
+System.out.println(atemp+" "+btemp);
+}
 }
