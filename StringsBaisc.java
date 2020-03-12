5df28352d371b3e4f9afd7db7ac5362a4a78e003
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-12 11:58:42 +0530

    main

--- StringsBaisc.java
+++ StringsBaisc.java
@@ -1,2 +1,11 @@
 import java.io.*;
 import java.util.*;
+public class StringsBasic {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+String A=sc.next();
+String B=sc.next();
+System.out.println(A.length()+B.length());
+int i = A.compareTo(B);
+
+}
