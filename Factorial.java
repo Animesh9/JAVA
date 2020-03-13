aa531c682a5e7bf01a0f9be4bd7d1ee84922c1f8
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-14 00:25:56 +0530

    Main

--- Factorial.java
+++ Factorial.java
@@ -3,6 +3,12 @@ class Factorial
 {
 public static void main(String args[] ) throws Exception {
 Scanner sc = new Scanner(System.in);
-
+int n = sc.nextInt();
+int pr=1;
+for( int i=1; i<=n; i++)
+{
+pr = pr * i;
+}
+System.out.println(pr);
 }
 }
