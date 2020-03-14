15b12fe53a229b7b44b444db0ef5ec863b0dfaaa
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-15 01:37:11 +0530

    Main

--- Palindrome.java
+++ Palindrome.java
@@ -2,5 +2,13 @@ import java.io.*;
 import java.util.Scanner;
 public class palindrome
 {
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String a=sc.nextLine();
+StringBuffer sb=new StringBuffer(a);
+sb.reverse();
+String s2 = sb.toString(); 
 
+}
 }
