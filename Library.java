663226ce254a6e33255fefb8310c8e93de8156e1
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-20 02:23:24 +0530

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,1 +1,15 @@
-
+//Assignment Question 3
+/*
+ Imagine a publishing company that markets both book and audio cassette versions of its works.
+ Create a class publication that stores the title (a string) and price (type float) of a publication.
+ From this class derive two classes: book, which adds a page count (type int), and tape, which adds
+ a playing time in minutes (type float). Each of these three classes should have a proper constructors,
+ destructors and display functions to display its data. Write a main() program to test (a) order of 
+ constructor and destructor invocation, (b) the book and tape classes by creating instances of them,
+ asking the user to fill in data and then displaying the data.  Start with the publication, book, and
+ tape classes of  ‘Ques-1’. Add an interface sales that declares functions to hold sales. Include a 
+ function to get sales amounts from the user, and a function to display the sales figures. Alter the
+ book and tape classes so they are derived from both publication and sales. An object of class book 
+ or tape should input and output sales data along with its other data. Write a main() class to create
+ a book object and a tape object and exercise their input/output capabilities. 
+ */
