1e252c3c5aeb9282c2ee5e6a4b67667c625f22d4
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-22 00:07:19 +0530

    Update SimpleArraySum.java

--- SimpleArraySum.java
+++ SimpleArraySum.java
@@ -4,4 +4,21 @@ import java.math.*;
 import java.text.*;
 import java.util.*;
 import java.util.regex.*;
+public class SimpleArraySum {
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    static int simpleArraySum(int[] ar) {
+        /*
+         * Write your code here.
+         */
+        int sum=0;  
+        for(int i=0;i<ar.length;i++)
+        {
+            sum+=ar[i];
+        }
+        return sum;
+
+    }
 
