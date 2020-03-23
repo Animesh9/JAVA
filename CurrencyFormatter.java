d1cdaebc0191923a7fe050bfa30648a18a9d14d1
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-24 01:22:24 +0530

    Main

--- CurrencyFormatter.java
+++ CurrencyFormatter.java
@@ -1,5 +1,19 @@
 import java.util.*;
 import java.text.*;
 public class CurrencyFormatter {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payement = scanner.nextDouble();
+        scanner.close();
+        
+        Locale obj = new Locale("en","IN");
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(obj);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        System.out.println("US: " + us.format(payement));
+        System.out.println("India: " + india.format(payement));
+        System.out.println("China: " + china.format(payement));
+        System.out.println("France: " + france.format(payement));
+    }
 }
