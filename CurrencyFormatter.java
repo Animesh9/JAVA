55515f29c067f106a3a33063836d8cb9cb8fba79
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-24 01:29:10 +0530

    HackerRank Code

--- CurrencyFormatter.java
+++ CurrencyFormatter.java
@@ -1,19 +1,24 @@
-import java.util.*;
-import java.text.*;
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class CurrencyFormatter {
+    
     public static void main(String[] args) {
+        /* Read input */
         Scanner scanner = new Scanner(System.in);
-        double payement = scanner.nextDouble();
+        double payment = scanner.nextDouble();
         scanner.close();
-        
-        Locale obj = new Locale("en","IN");
-        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
-        NumberFormat india = NumberFormat.getCurrencyInstance(obj);
-        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        Locale indiaLocale = new Locale("en", "IN");
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
         NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        System.out.println("US: " + us.format(payement));
-        System.out.println("India: " + india.format(payement));
-        System.out.println("China: " + china.format(payement));
-        System.out.println("France: " + france.format(payement));
+        System.out.println("US: "     + us.format(payment));
+        System.out.println("India: "  + india.format(payment));
+        System.out.println("China: "  + china.format(payment));
+        System.out.println("France: " + france.format(payment));
     }
 }
+
+//..HackerRank Code
