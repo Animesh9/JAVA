84af48933da6a68c3c710fae612b3a87221dcf1d
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-25 10:24:48 +0530

    class B implements Runnable

--- ThreadEx1.java
+++ ThreadEx1.java
@@ -7,3 +7,12 @@ for(i=0;i<=4;i++)
 System.out.println("Thread A : "+ i);
 }
 }
+class B implements Runnable
+{
+public void run()
+{
+int i;
+for(i=0;i<=4;i++)
+System.out.println("Thread B : "+ i);
+}
+}
