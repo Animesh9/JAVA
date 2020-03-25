cf8205ceabf0576293789f7d780f8a7125f438ec
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-25 10:25:08 +0530

    Main

--- ThreadEx1.java
+++ ThreadEx1.java
@@ -16,3 +16,13 @@ for(i=0;i<=4;i++)
 System.out.println("Thread B : "+ i);
 }
 }
+public class ThreadEx1
+{
+public static void main(String args [])
+{
+Thread t1 = new Thread(new A());
+Thread t2 = new Thread(new B()); 
+t1.start();
+t2.start();
+}
+}
