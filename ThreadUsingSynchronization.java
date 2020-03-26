294c875ca827f9fa04c512e7f455f2e57f00ada1
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-27 03:22:35 +0530

    Update ThreadUsingSynchronization.java

--- ThreadUsingSynchronization.java
+++ ThreadUsingSynchronization.java
@@ -42,3 +42,15 @@ System.out.println("Insufficient Balance");
 }
 }
 }
+public class ThreadUsingSynchronization
+{
+public static void main (String args[])
+{
+Account a1 = new Account(1000);
+Customer c1 = new Customer(a1,"Ani") , c2 = new Customer(a1,"Animesh");
+Thread t1 = new Thread(c1);
+Thread t2 = new Thread(c2);
+t1.start();
+t2.start();
+}
+}
