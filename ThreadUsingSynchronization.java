d66f1dfd21fe626da79f5900c7ccb700611d0fa0
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-27 03:20:37 +0530

    Update ThreadUsingSynchronization.java

--- ThreadUsingSynchronization.java
+++ ThreadUsingSynchronization.java
@@ -1,1 +1,20 @@
 import java.util.Scanner;
+class Account
+{
+private int bal;
+public Account(int bal)
+{this.bal=bal;}
+public boolean isSufbal(int w)
+{
+if (bal>w)
+return(true);
+else
+return(false);
+}
+public void withdraw(int amt)
+{
+bal=bal-amt;
+System.out.println("Withdrawl mone is "+amt);
+System.out.println("Your current balance is "+bal);
+}
+}
