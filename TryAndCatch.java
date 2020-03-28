76acab10f6dec667a92d324eb1fadaf53021217f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-29 04:41:12 +0530

    Update TryAndCatch.java

--- TryAndCatch.java
+++ TryAndCatch.java
@@ -3,6 +3,12 @@ class TryAndCatch
 {
 public static void main(String args[])
 {
+Scanner s = new Scanner(System.in);
+int a , b ,c=0;
+System.out.println("Enter value of a : ");
+a = s.nextInt();
+System.out.println("Enter value of b : ");
+b = s.nextInt();
 
 }
 }
