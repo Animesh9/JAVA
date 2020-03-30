a8cd9f21c459519873f932984d4f33d0b40ea2dd
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-31 00:30:07 +0530

    Update InttoString.java

--- InttoString.java
+++ InttoString.java
@@ -10,7 +10,9 @@ public class Solution {
    int n = in .nextInt();
    in.close();
    //String s=???; Complete this line below
-   
+      //Write your code here
+    String s = String.valueOf(n);
+
    if (n == Integer.parseInt(s)) {
     System.out.println("Good job");
    } else {
