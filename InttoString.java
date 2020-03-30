cf84ae1c3e382bd7bdc7d845d72eb0f37653bbda
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-31 00:29:19 +0530

    Update and rename JavaInttoString.java to InttoString.java

---
+++ InttoString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
