ab4ed108c0baa6605786effa72642d858737ea5f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-01 03:09:53 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -2,7 +2,16 @@ public class Armstrong {
     public static void main(String[] args) {
 
         int number = 371, originalNumber, remainder, result = 0;
+        originalNumber = number;
 
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+
+        
 
     }
 }
