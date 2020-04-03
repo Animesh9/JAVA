3e46158e17aab96a9ba09153b9dbf915df2c5f86
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-04 01:53:33 +0530

    Update ExtThreadAndImpleInter.java

--- ExtThreadAndImpleInter.java
+++ ExtThreadAndImpleInter.java
@@ -7,5 +7,10 @@ System.out.println("In test class ");
 }
 class ExtThreadAndImpleInter extends test implements Runnable
 {
-
+public void run()
+{
+System.out.println("Child method to be executed by child thread");
+}
+public static void main(String [] args)
+{
 }
