569255ac856b018ba5a0fa9ac344924c54977b75
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-03 11:34:25 +0530

    main

--- SearchChar.java
+++ SearchChar.java
@@ -4,6 +4,13 @@ class SearchChar
 { 
 public static void main (String[] args) 
 {
-
-} 
+Scanner s = new Scanner(System.in);
+String str = s.nextLine();
+char a = s.next().charAt(0);
+int firstIndex = str.indexOf(a); 
+System.out.println("First occurrence of char 'a'" + " is found at : " + firstIndex); 
+char b = s.next().charAt(0);
+int lastIndex = str.lastIndexOf(b); 
+System.out.println("Last occurrence of char 'a' is" + " found at : " + lastIndex);
+}
 }
