30bd7d41116417c1f4b4562f7ea17abec55fc992
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-05 03:00:25 +0530

    Update CRC.java

--- CRC.java
+++ CRC.java
@@ -13,3 +13,20 @@ m=sc.nextInt();
 d=new int[n+m];
 g=new int[m];
 System.out.print("Enter data bits : ");
+for(i=0;i<n;i++)
+d[i]=sc.nextInt();
+System.out.print("Enter generator bits : ");
+for(j=0;j<m;j++)
+g[j]=sc.nextInt();
+for(i=0;i<m-1;i++)
+d[n+i]=0;
+r=new int[m+n];
+for(i=0;i<m;i++)
+r[i]=d[i];
+z=new int[m];
+for(i=0;i<m;i++)
+z[i]=0;
+for(i=0;i<n;i++)
+{
+k=0;
+msb=r[i];
