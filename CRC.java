bc477be18f82110d6e06dbb7d1972da3cf38e74c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-05 03:00:00 +0530

    Update CRC.java

--- CRC.java
+++ CRC.java
@@ -1,2 +1,15 @@
 //Code for Cyclic Redundancy Check.
 import java.util.Scanner;
+class CRC
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int m,g[],n,d[],z[],r[],msb,i,j,k;
+System.out.print("Enter no. of data bits : ");
+n=sc.nextInt();
+System.out.print("Enter no. of generator bits : ");
+m=sc.nextInt();
+d=new int[n+m];
+g=new int[m];
+System.out.print("Enter data bits : ");
