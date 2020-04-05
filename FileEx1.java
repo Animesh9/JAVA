9afa88cbc2b5eeeb8f8f29d1432ea7c38e6a592c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-06 01:10:39 +0530

    Update FileEx1.java

--- FileEx1.java
+++ FileEx1.java
@@ -1,5 +1,12 @@
 import java.io.*;
 public class FileEx1
 {
+public static void main(String [] args) throws IOException
+{
+File f1 = new File("d:/java/file1.txt");
+f1.createNewFile();
+System.out.println("Check if file exist or not.\nFile exist " + f1.exists());
+System.out.println("File Size " + f1.length());
 
+}
 }
