0142b0a5797c0c2258bb0ce68c26ab371eaf6b36
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-07 00:29:57 +0530

    Update FileEx2.java

--- FileEx2.java
+++ FileEx2.java
@@ -4,7 +4,7 @@ public class FileEx2
 public static void main(String [] args) throws IOException
 {
 int i;
-FileOutputStream fout = new FileOutputStream("d:/java/myfile.txt",true);
+FileOutputStream fout = new FileOutputStream("d:/java/myfile.txt",true);  //This is path in my system
 String s= "new day new way";
 char ch[] = s.toCharArray();
 for(i=0 ; i<s.length() ; i++)
