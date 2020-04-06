85280a669272729149759376cb9f8058c907a748
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-07 00:30:19 +0530

    Update FileEx1.java

--- FileEx1.java
+++ FileEx1.java
@@ -3,7 +3,7 @@ public class FileEx1
 {
 public static void main(String [] args) throws IOException
 {
-File f1 = new File("d:/java/file1.txt");
+File f1 = new File("d:/java/file1.txt");   // this path is in my system
 f1.createNewFile();
 System.out.println("Check if file exist or not.\nFile exist " + f1.exists());
 System.out.println("File Size " + f1.length());
