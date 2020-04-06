c111ac23090fba075ca229656c05ec9de8d624ae
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-07 00:29:33 +0530

    main

--- FileEx2.java
+++ FileEx2.java
@@ -3,5 +3,12 @@ public class FileEx2
 {
 public static void main(String [] args) throws IOException
 {
-
+int i;
+FileOutputStream fout = new FileOutputStream("d:/java/myfile.txt",true);
+String s= "new day new way";
+char ch[] = s.toCharArray();
+for(i=0 ; i<s.length() ; i++)
+fout.write(ch[i]);
+fout.close();
+}
 }
