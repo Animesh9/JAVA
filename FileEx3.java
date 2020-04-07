761dfeab02eade4f76f82dfd2ce748ed9d2b9a0a
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-08 00:56:00 +0530

    Update FileEx3.java

--- FileEx3.java
+++ FileEx3.java
@@ -1,1 +1,12 @@
 import java.io.*;
+public class FileEx3
+{
+public static void main(String [] args) throws IOException
+{
+int i;
+FileOutputStream fout = new FileOutputStream("d:/java/file2.txt",true);
+FileInputStream fin = new FileInputStream("d:/java/file2.txt");
+String s= "new day new way";
+
+}
+}
