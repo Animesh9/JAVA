e0062070fcc8b52570f357e09e1ea892ae6bb836
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-10 00:07:02 +0530

    Update GenericMethod.java

--- GenericMethod.java
+++ GenericMethod.java
@@ -8,6 +8,9 @@ System.out.println(x);
 public static void main(String args[])
 {
 GenericMethod gm1 = new GenericMethod();
-
+String people[] = new String[]{"Ani","Animesh"};
+Integer number[]={9,16,45};
+gm1.printArray(people);
+gm1.printArray(number);
 }
 }
