1245a4c2c5d107f48f0d52e448a0458f9f0ed2dc
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-10 00:06:53 +0530

    Update GenericMethod.java

--- GenericMethod.java
+++ GenericMethod.java
@@ -1,4 +1,13 @@
 public class GenericMethod
 {
+public <A> void printArray(A []s)
+{
+for (A x:s)
+System.out.println(x);
+}
+public static void main(String args[])
+{
+GenericMethod gm1 = new GenericMethod();
 
+}
 }
