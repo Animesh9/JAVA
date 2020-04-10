bdbbe5356aa27fc7ff4075eaedf36ccc7f0ecee8
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-11 03:46:38 +0530

    Update FileEx5.java

--- FileEx5.java
+++ FileEx5.java
@@ -2,6 +2,10 @@ import java.io.*;
 public class FileEx5{
 public static void main(String args []) throws IOException
 {
-
+int ch ;
+BufferedReader bf = new BufferedReader(new FileReader("d:/java/file3.txt"));
+char []s = new char[20];
+bf.read(s,2	,15);
+bf.close();
 }
 }
