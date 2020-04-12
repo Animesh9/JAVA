985911a468a3495d49747c9280e367c5ab15da66
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-12 05:42:03 +0530

    Update GenericClass.java

--- GenericClass.java
+++ GenericClass.java
@@ -16,6 +16,9 @@ public static void main(String args[])
 {
 Data<Integer> g1 = new Data<Integer>();
 Data<String> g2 = new Data<String>();
-
+Integer k = new Integer(5);
+g1.add(k);
+g2.add("Animesh");
+System.out.println("g1 = " +g1.get() +"\ng2 = "+g2.get());
 }
 }
