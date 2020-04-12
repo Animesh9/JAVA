67494cfc48c963237d3c356c2ca3d4e8cec750f6
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-13 00:32:03 +0530

    basics of collectio(main)

--- CollectionEx1.java
+++ CollectionEx1.java
@@ -8,6 +8,10 @@ h.add(20);
 h.add(10);
 h.add(60);
 h.add(40);
-
+int x= Collections.binarySearch(h,10);
+System.out.println(h);
+Collection.sort(h);
+System.out.println(h);
+System.out.println(x);
 }
 }
