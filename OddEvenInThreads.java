107f9d2aac098c842c5521d69d6495d4d8870b79
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-15 00:42:45 +0530

    Update OddEvenInThreads.java

--- OddEvenInThreads.java
+++ OddEvenInThreads.java
@@ -6,3 +6,16 @@ even()
 t=new Thread(this);
 start();
 }
+public void run()
+{
+try
+{
+for(int i=2;i<50;i+=2)
+System.out.print(i+" ");
+Thread.sleep(100);
+}
+catch(Exception e)
+{System.out.println("thread interepted");
+}
+}
+}
