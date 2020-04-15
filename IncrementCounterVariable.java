5606316202874455f7adca175bd2bfd2335bdfba
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-16 01:27:24 +0530

    Update IncrementCounterVariable.java

--- IncrementCounterVariable.java
+++ IncrementCounterVariable.java
@@ -2,6 +2,19 @@ class IncrementCounterVariable
 {
 public static void main(String arg[])throws Exception
 {
+data d1=new data();
+data d2=new data();
+data d3=new data();
+data d4=new data();
+data d5=new data();
+data d6=new data();
+data d7=new data();
+data d8=new data();
+data d9=new data();
+data d10=new data();
+System.out.println(d10.count);
+}
+}
 
 }
 }
