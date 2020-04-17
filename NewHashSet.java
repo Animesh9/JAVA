51a7942cd33591f9b7a68a016c0709776d01f269
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-18 03:14:39 +0530

    Update NewHashSet.java

--- NewHashSet.java
+++ NewHashSet.java
@@ -3,5 +3,12 @@ import java.util.HashSet;
 import java.util.List;
 class NewHashSet{
 public static void main(String agr[]){
+List names = new ArrayList<String>();
+names.add("Animesh");
+names.add("Ani");
+names.add("Kush");
+names.add("Roman");
+names.add("Chirag");
 
+}
 }
