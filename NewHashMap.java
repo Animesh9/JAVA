b583b1ab84bce1ec57b2a7197d29912d8d26a9c5
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-17 09:36:04 +0530

    Update NewHashMap.java

--- NewHashMap.java
+++ NewHashMap.java
@@ -1,3 +1,18 @@
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+class NewHashMap
+{
+public static void main(String args[])
+{
+HashMap<String, Integer> map = new HashMap<>(); 
+map.put("Animesh", 10); 
+map.put("Ani", 30); 
+map.put("Roman", 20);
+map.put("Kush", 20);
+System.out.println();
+String key = "Animesh";
+
+}  
+} 
+} 
