2ef98999366b21f0b2bebca9f97231a4f37dce46
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-17 09:36:35 +0530

    Update NewHashMap.java

--- NewHashMap.java
+++ NewHashMap.java
@@ -12,6 +12,18 @@ map.put("Roman", 20);
 map.put("Kush", 20);
 System.out.println();
 String key = "Animesh";
+if (map.containsKey(key)) 
+{
+Integer a = map.get(key); 
+System.out.println("value for key "+ key  +" is: "+a); 
+}
+int value = 20; 
+System.out.println();
+if (map.containsValue(value)) 
+{
+Iterator it = map.entrySet().iterator();
+while(it.hasNext()){ 
+String str = it.next().toString();
 
 }  
 } 
