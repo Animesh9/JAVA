4dfeb9e9e9eb39c9bc97d7af0731c8180b66e4f3
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-19 01:02:14 +0530

    Update JavaCollection.java

--- JavaCollection.java
+++ JavaCollection.java
@@ -3,3 +3,15 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+class JavaCollection
+{
+public static void main(String args[])
+{
+List names = new ArrayList<String>();
+names.add("Animesh");
+names.add("Ani");
+names.add("Kush");
+names.add("Roman");
+
+}
+}
