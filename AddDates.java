087f6027439fbc092d68774f5c131b7057bb3dc0
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-20 00:41:03 +0530

    Update AddDates.java

--- AddDates.java
+++ AddDates.java
@@ -1,5 +1,14 @@
 import java.util.Calendar;
 public class AddDates 
 {
+public static void main(String[] args) 
+{
+Calendar c1 = Calendar.getInstance();
+Calendar c2 = Calendar.getInstance();
+Calendar cTotal = (Calendar) c1.clone();
+cTotal.add(Calendar.YEAR, c2.get(Calendar.YEAR));
+cTotal.add(Calendar.MONTH, c2.get(Calendar.MONTH) + 1); // Zero-based months
+cTotal.add(Calendar.DATE, c2.get(Calendar.DATE));
 
+}
 }
