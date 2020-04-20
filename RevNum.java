fea85e6c1a8b4ef02f27b0d62a3c3bfaa6ea0a3a
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-21 00:11:14 +0530

    main

--- RevNum.java
+++ RevNum.java
@@ -4,7 +4,11 @@ public static void main(String[] args)
 {
 int num = 1234, reversed = 0;
 while(num != 0) 
-
+{
+int digit = num % 10;
+reversed = reversed * 10 + digit;
+num /= 10;
+}
 System.out.println("Reversed Number: " + reversed);
 }
 }
