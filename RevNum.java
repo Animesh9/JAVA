7f7bc573bbbda7e310f1a88abbd4bacc558a0977
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-21 00:10:58 +0530

    Update RevNum.java

--- RevNum.java
+++ RevNum.java
@@ -2,5 +2,9 @@ public class ReverseNumber
 {
 public static void main(String[] args) 
 {
+int num = 1234, reversed = 0;
+while(num != 0) 
 
+System.out.println("Reversed Number: " + reversed);
+}
 }
