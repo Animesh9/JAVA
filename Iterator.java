81cc55990d6dde8ed5118fb572ad97fa1f94c847
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-23 00:31:57 +0530

    Main

--- Iterator.java
+++ Iterator.java
@@ -8,6 +8,14 @@ newSet.add("1");
 newSet.add("2");
 newSet.add("3");
 Iterator<String> itr = newSet.iterator();
-
+while(itr.hasNext()) 
+{
+System.out.println(itr.next());
+}
+itr.remove();
+for(String var:newSet) 
+{
+System.out.println(var);
+}
 }
 }
