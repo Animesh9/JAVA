90e85a134b05bdb2373b6b0bd294356625795e24
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-24 00:25:02 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,2 +1,12 @@
 import java.io.*;
 import java.util.*;
+public class ReverseString {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+    }
+}
