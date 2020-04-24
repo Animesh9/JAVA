2f5df03abcca88539d9d4c0915dd669ec96073dd
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-25 01:40:43 +0530

    Update GUIEx1.java

--- GUIEx1.java
+++ GUIEx1.java
@@ -11,6 +11,12 @@ class n extends JFrame
 {
 public n()
 {
-
+JLabel l = new JLabel("Welcome Animesh9");
+add(l);
+setLayout(new FlowLayout());
+setVisible(true);
+setSize(700,500);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
 }
+// ... Basic GUI ...
