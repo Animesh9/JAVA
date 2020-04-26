e5a874f01c4de1cd84c8309cd31ee93a803123a1
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-27 00:25:01 +0530

    Update StringIntro.java

--- StringIntro.java
+++ StringIntro.java
@@ -7,6 +7,8 @@ public class Solution {
         String A=sc.next();
         String B=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-
+        System.out.println(A.length()+B.length());
+        System.out.println(A.compareTo(B)>0?"Yes":"No");
+        System.out.println(A.substring(0, 1).toUpperCase()+A.substring(1, A.length())+" "+B.substring(0, 1).toUpperCase()+B.substring(1, B.length()));        
     }
 }
