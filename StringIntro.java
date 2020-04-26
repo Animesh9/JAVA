bbbd8e8dac6d911a70c07d6393e1a3c1ad1f8f60
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-27 00:24:52 +0530

    Update StringIntro.java

--- StringIntro.java
+++ StringIntro.java
@@ -1,2 +1,12 @@
 import java.io.*;
 import java.util.*;
+public class Solution {
+    public static void main(String[] args) 
+    {        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+    }
+}
