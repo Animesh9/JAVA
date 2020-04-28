38075c63493476dcddb299ff86f0d0df69885abd
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-28 13:14:31 +0530

    HackerRank Code

--- 1DArray.java
+++ 1DArray.java
@@ -7,10 +7,16 @@ public class 1DArray {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            a[i]=scan.nextInt();
+        }
+        scan.close();
         // Prints each sequential element in array a
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
     }
 }
+//...HackerRank code for 1D Array...
