f085745e6dfa3c5175c272f886eb4495ad2ad635
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-28 13:13:19 +0530

    Update and rename 1Darray.java to 1DArray.java

---
+++ 1DArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class 1DArray {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
