a687601429e5a88f9b5b0ca5dcc4b9555496e055
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-04-29 00:52:23 +0530

    Update JButtonEx2.java

--- JButtonEx2.java
+++ JButtonEx2.java
@@ -1,2 +1,13 @@
 import java.awt.event.*;  
 import javax.swing.*;    
+public class JButtonEx2 
+{  
+public static void main(String[] args) 
+{  
+JFrame f=new JFrame("JButtonEx2");  
+final JTextField t = new JTextField();  
+t.setBounds(50, 50, 150, 20);  
+JButton b = new JButton("Click Here");  
+b.setBounds(50,100,95,30);  
+b.addActionListener(new ActionListener()
+{
