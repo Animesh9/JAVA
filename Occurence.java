3e560aeb3c70a215ea19782d0ba78947cb196a75
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-07 01:21:46 +0530

    Update Occurence.java

--- Occurence.java
+++ Occurence.java
@@ -1,4 +1,11 @@
 class Occurence 
 { 
- 
+ public static void main (String[] args) 
+{ 
+String str = "Hey! This is Animesh Jain.";  
+int firstIndex = str.indexOf('s'); 
+System.out.println("First occurrence of char 's'" + " is found at : " + firstIndex); 
+int lastIndex = str.lastIndexOf('s'); 
+
+}
 }
