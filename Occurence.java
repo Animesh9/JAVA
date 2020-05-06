e5bd38c8320196bc0be5ef90bee1074a7f2ea289
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-07 01:22:18 +0530

    Main

--- Occurence.java
+++ Occurence.java
@@ -11,6 +11,7 @@ int first_in = str.indexOf('s', 10);
 System.out.println("First occurrence of char 's'" + " after index 10 : " + first_in); 
 int last_in = str.lastIndexOf('s', 20); 
 System.out.println("Last occurrence of char 's'" + " after index 20 is : " + last_in); 
- 
+int char_at = str.charAt(20); 
+System.out.println("Character at location 20: " + char_at);
 }
 }
