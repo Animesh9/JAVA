5943ec1890db4994ed8401f1fc308285a8c0222c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-07 01:21:59 +0530

    Update Occurence.java

--- Occurence.java
+++ Occurence.java
@@ -6,6 +6,11 @@ String str = "Hey! This is Animesh Jain.";
 int firstIndex = str.indexOf('s'); 
 System.out.println("First occurrence of char 's'" + " is found at : " + firstIndex); 
 int lastIndex = str.lastIndexOf('s'); 
-
+System.out.println("Last occurrence of char 's' is" + " found at : " + lastIndex); 
+int first_in = str.indexOf('s', 10); 
+System.out.println("First occurrence of char 's'" + " after index 10 : " + first_in); 
+int last_in = str.lastIndexOf('s', 20); 
+System.out.println("Last occurrence of char 's'" + " after index 20 is : " + last_in); 
+ 
 }
 }
