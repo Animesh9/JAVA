2b99610480995e40a5dc7ae750536419c55ebfde
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-10 23:58:36 +0530

    Update ConvertingType1.java

--- ConvertingType1.java
+++ ConvertingType1.java
@@ -1,4 +1,11 @@
 public class ConvertingType1 
 {
+public static void main(String[] args) 
+{
+Integer num = new Integer(20);
+System.out.println(num.intValue());
+int c = 1234;
+String string = String.valueOf(c);
 
+}
 }
