14c63f821e6663ccd6b6de2e46c467e56a84aca2
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-12 00:21:31 +0530

    Update NameLocation.java

--- NameLocation.java
+++ NameLocation.java
@@ -1,4 +1,13 @@
 class data
 {
+String name;
+data(String n){ name=n; }
+void disp()
+{
+System.out.println("_______________________________");
+System.out.println("Name :"+name);
+int c=0;
+int len=name.length();
 
+}
 }
