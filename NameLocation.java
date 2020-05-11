24606a8446fbd2db299820a34dc31ff5c61fc1fa
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-12 00:21:47 +0530

    Update NameLocation.java

--- NameLocation.java
+++ NameLocation.java
@@ -8,6 +8,13 @@ System.out.println("_______________________________");
 System.out.println("Name :"+name);
 int c=0;
 int len=name.length();
+for(int i=0;i<len;i++)
+if(name.charAt(i)=='A'||name.charAt(i)=='a')
+{
+c++;
+System.out.println("number of occurance :"+c);
+System.out.println("Possition :"+(i+1));
+}
 
 }
 }
