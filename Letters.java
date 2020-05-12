1feccbb8bc91df936e3902b5f96edb9965b1c366
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-13 00:22:27 +0530

    Class Letters

--- Letters.java
+++ Letters.java
@@ -1,3 +1,16 @@
 class Letters{ 
+static void convertOpposite(StringBuffer str) 
+{ 
+int ln = str.length(); 
+for (int i=0; i<ln; i++) 
+{ 
+Character c = str.charAt(i); 
+if (Character.isLowerCase(c)) 
+str.replace(i, i+1, Character.toUpperCase(c)+""); 
+else
+str.replace(i, i+1, Character.toLowerCase(c)+""); 
+} 
+} 
+
 
 } 
