c62062175c2e593453aec26417a55c63481d8695
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-14 00:11:28 +0530

    Update Exception1.java

--- Exception1.java
+++ Exception1.java
@@ -8,7 +8,19 @@ int[] rollno = new int[5];
 public class Exception1 {
 public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
 exp7 obj = new exp7();
-
+for (int i = 0; i < obj.rollno.length; i++) {
+try { // identification of error in try block
+Scanner in = new Scanner(System.in);
+System.out.println("Enter the name of the student");
+obj.name[i] = in.nextLine();
+System.out.println("Enter the roll no of the student");
+obj.rollno[i] = in.nextInt();
+}
+catch (ArrayIndexOutOfBoundsException e) // catching the errors
+{
+System.out.println("index is exceeding");
+}
+}
 System.out.println(Arrays.toString(obj.name));
 System.out.println(Arrays.toString(obj.rollno));
 }
