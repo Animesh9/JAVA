f5224935d9aeaff1e21691538dc52e64855f8071
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-14 00:10:45 +0530

    exception

--- Exception1.java
+++ Exception1.java
@@ -2,5 +2,16 @@ import java.lang.ArrayIndexOutOfBoundsException;
 import java.util.Arrays;
 import java.util.Scanner;
 class exp7 {
+String[] name = new String[5];
+int[] rollno = new int[5];
+}
+public class Exception1 {
+public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+exp7 obj = new exp7();
+for (int i = 0; i < obj.rollno.length; i++) {
+try { // identification of error in try block
+Scanner in = new Scanner(System.in);
+System.out.println("Enter the name of the student");
 
+}
 }
