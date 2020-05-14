c508621021315648ceecb4a0fc4643565f4bbd3f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-15 01:50:08 +0530

    Update PriorityInThreads.java

--- PriorityInThreads.java
+++ PriorityInThreads.java
@@ -6,5 +6,14 @@ System.out.println(Thread.currentThread().getName());
 System.out.println("Threading is done" + Thread.currentThread().getPriority());
 }
 }
+public class PriorityInThreads {
+public static void main(String[] args) {
+que m = new que();
+Thread t1 = new Thread(m);
+Thread t2 = new Thread(m);
+Thread t3 = new Thread(m);
+Thread t4 = new Thread(m);
+Thread t5 = new Thread(m);
 
+}
 }
