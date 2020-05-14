db75b9cab7507a944fde60bf4cac9d3819b6261c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-15 01:49:35 +0530

    Update PriorityInThreads.java

--- PriorityInThreads.java
+++ PriorityInThreads.java
@@ -1,4 +1,10 @@
 class que implements Runnable 
 {
+public synchronized void run() 
+{
+System.out.println(Thread.currentThread().getName());
+System.out.println("Threading is done" + Thread.currentThread().getPriority());
+}
+}
 
 }
