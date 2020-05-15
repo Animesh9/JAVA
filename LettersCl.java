4654019d4763dd68fce24afae76c31b71a530132
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-16 01:56:57 +0530

    Update LettersCl.java

--- LettersCl.java
+++ LettersCl.java
@@ -1,5 +1,10 @@
 import java.io.*;
 class LettersCL
 {
+public static void main(String a[]) throws IOException
+{
+DataInputStream in=new DataInputStream(System.in);
+System.out.println("Enter Statement:");
 
+}
 }
