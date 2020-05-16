df024ef0bcde6717a38ea988cf29f4d4e03ba8b8
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-17 02:05:23 +0530

    Update Exception2.java

--- Exception2.java
+++ Exception2.java
@@ -1,4 +1,18 @@
 import java.util.Scanner;
 class demoexception {
-
+int num1, num2;
+void dividefunction() {
+System.out.println("Enter two numbers: ");
+Scanner in = new Scanner(System.in);
+num1 = in.nextInt();
+num2 = in.nextInt();
+try { 
+int result = num1 / num2;
+System.out.println(result);
+} catch (ArithmeticException e) 
+{ 
+System.out.println("A number can not be divided by 0");
+}
+}
+}
 }
