f3594bba203cd986d304f5f9d0cb51393efe2043
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-18 00:46:42 +0530

    Update Exception3.java

--- Exception3.java
+++ Exception3.java
@@ -2,6 +2,12 @@ import java.util.*;
 class Exception3{
 public static void main(String[] args) {
 int x,y;
-
+try{
+x = Integer.parseInt(args[0]);
+y = Integer.parseInt(args[1]);
+}    
+catch(NumberFormatException e){
+System.out.println("Not an Integer!");
+}
 }
 }
