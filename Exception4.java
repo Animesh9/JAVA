4874a662fa7884de454fc5052f49b9f1cb40db8c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-19 00:12:32 +0530

    Update Exception4.java

--- Exception4.java
+++ Exception4.java
@@ -5,5 +5,10 @@ super("collision");
 }
 }
 public class Exception4{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the direction same or opposite");
+String a=sc.nextLine();
+
 }
 }
