d592b256f09ac0487c38c22a95232a689184da40
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-19 00:12:47 +0530

    Update Exception4.java

--- Exception4.java
+++ Exception4.java
@@ -9,6 +9,21 @@ public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 System.out.println("enter the direction same or opposite");
 String a=sc.nextLine();
-
+try
+{
+if (a.equals("opposite"))
+{
+throw new direction();
+}
+else
+{
+System.out.println("no problem");
+}
+}
+catch(direction e)
+{
+System.out.println(e);
+}
+System.out.println("try another time");
 }
 }
