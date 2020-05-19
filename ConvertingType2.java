c264d772e314355e41f7612f773871a32f0b75c4
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-20 02:25:55 +0530

    Update ConvertingType2.java

--- ConvertingType2.java
+++ ConvertingType2.java
@@ -5,6 +5,12 @@ public static void main(String[] args)
 Float fObj = new Float("78.50");
 byte b = fObj.byteValue();
 System.out.println(b);
+short s = fObj.shortValue();
+System.out.println(s);
+int i = fObj.intValue();
+System.out.println(i);
+float f = fObj.floatValue();
+System.out.println(f);
 
 }
 }
