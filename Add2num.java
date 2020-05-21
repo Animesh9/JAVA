27c8ea89a04ca3cdd9797c05c8ac1f519230a4d5
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-22 01:56:03 +0530

    Update Add2num.java

--- Add2num.java
+++ Add2num.java
@@ -1,10 +1,11 @@
 //This program will add 2 numbers using command line arguments
-public class Add2Num{
-public static void main (String[] args)
+public class Add2Num
 {
-int a=Integer.parseInt(args[0]);//argument 1
-int b=Integer.parseInt(args[1]);//argument 2
-int c=a+b;//adding arguments
-System.out.println("Add is:"+c);
-}
+  public static void main (String[] args)
+  {
+    int a=Integer.parseInt(args[0]);//argument 1
+    int b=Integer.parseInt(args[1]);//argument 2
+    int c=a+b;//adding arguments
+    System.out.println("Add is:"+c);
+  }
 }
