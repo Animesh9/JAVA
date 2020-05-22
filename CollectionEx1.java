a520a8437fa5974e7e7a4d93bf3beb2af3be8728
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-23 02:37:49 +0530

    Update CollectionEx1.java

--- CollectionEx1.java
+++ CollectionEx1.java
@@ -1,17 +1,17 @@
 import java.util.*;
 public class CollectionEx1
 {
-public static void main(String [] args)
-{
-Vector h = new Vector();
-h.add(20);
-h.add(10);
-h.add(60);
-h.add(40);
-int x= Collections.binarySearch(h,10);
-System.out.println(h);
-Collection.sort(h);
-System.out.println(h);
-System.out.println(x);
-}
+  public static void main(String [] args)
+  {
+    Vector h = new Vector();
+    h.add(20);
+    h.add(10);
+    h.add(60);
+    h.add(40);
+    int x= Collections.binarySearch(h,10);
+    System.out.println(h);
+    Collection.sort(h);
+    System.out.println(h);
+    System.out.println(x);
+  }
 }
