eb3c63e42b7d146f0e1f0d49d6e9aa2e55964d59
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-23 02:36:46 +0530

    Update BiggestNum.java

--- BiggestNum.java
+++ BiggestNum.java
@@ -1,12 +1,14 @@
 //Find biggest of three number 
-public class BiggestNum {
-public static void main(String args[]) {
-int num1 = 15 , num2 = 25 , num3 = 41;
-if (num1 >= num2 && num1 >= num3)//this will check if num1 is greater than 2 or three
-System.out.println( num1 + " is the maximum number.");
-else if (num2 >= num1 && num2 >= num3)// if num2 is greater than num3 and num1
-System.out.println( num2 + " is the maximum number.");
-else//if num 3 is greatest
-System.out.println( num3 + " is the maximum number.");
-}
+public class BiggestNum 
+{
+  public static void main(String args[]) 
+    {
+      int num1 = 15 , num2 = 25 , num3 = 41;
+      if (num1 >= num2 && num1 >= num3)//this will check if num1 is greater than 2 or three
+      System.out.println( num1 + " is the maximum number.");
+      else if (num2 >= num1 && num2 >= num3)// if num2 is greater than num3 and num1
+      System.out.println( num2 + " is the maximum number.");
+      else//if num 3 is greatest
+      System.out.println( num3 + " is the maximum number.");
+    }
 }
