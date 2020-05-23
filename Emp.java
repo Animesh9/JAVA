a70001e4fd5ef0d1bc7b9f4e85fff0cb04db3c94
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-24 00:07:47 +0530

    Update Emp.java

--- Emp.java
+++ Emp.java
@@ -1,1 +1,22 @@
 import java.util.*;
+class employee
+{
+String name;
+int empid;
+int salary;
+employee()
+{
+name="bajaj";
+empid=67;
+salary=9000;
+}
+employee(String n,int e,int s)
+{
+name=n;
+empid=e;
+salary=s;
+}
+String no()
+{
+return name;
+}
