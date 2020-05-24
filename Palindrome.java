e5833b887e3bf45771ac2351c8cd2c1c2b3dc3a2
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-25 01:58:19 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -2,20 +2,20 @@ import java.io.*;
 import java.util.Scanner;
 public class palindrome
 {
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
-String a=sc.nextLine();
-StringBuffer sb=new StringBuffer(a);
-sb.reverse();
-String s2 = sb.toString(); 
-if(a.equals(s2))
-{
-System.out.println("YES");
-}
-else
-{
-System.out.println("NO");
-}
-}
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String a=sc.nextLine();
+    StringBuffer sb=new StringBuffer(a);
+    sb.reverse();
+    String s2 = sb.toString(); 
+    if(a.equals(s2))
+    {
+      System.out.println("YES");
+    }
+    else
+    {
+      System.out.println("NO");
+    }
+  }
 }
