3eb76c1735858bf063ce4cfa0327055885a2a588
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-25 01:59:39 +0530

    Update TryAndCatch.java

--- TryAndCatch.java
+++ TryAndCatch.java
@@ -2,21 +2,22 @@
 import java.util.Scanner;
 class TryAndCatch
 {
-public static void main(String args[])
-{
-Scanner s = new Scanner(System.in);
-int a , b ,c=0;
-System.out.println("Enter value of a : ");
-a = s.nextInt();
-System.out.println("Enter value of b : ");
-b = s.nextInt();
-try{
-c=a/b;
-System.out.println(a + " divide by "+ b +": = " + c);
-}
-catch(ArithmeticException e)
-{
-System.out.println("Exception : " + e.getMessage());
-}
-}
+  public static void main(String args[])
+  {
+    Scanner s = new Scanner(System.in);
+    int a , b ,c=0;
+    System.out.println("Enter value of a : ");
+    a = s.nextInt();
+    System.out.println("Enter value of b : ");
+    b = s.nextInt();
+    try
+    {
+      c=a/b;
+      System.out.println(a + " divide by "+ b +": = " + c);
+    }
+    catch(ArithmeticException e)
+    {
+      System.out.println("Exception : " + e.getMessage());
+    }
+  }
 }
