c132e64e11956f5fa63eba52c17211750c9372ef
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-27 02:54:18 +0530

    Update ThreadEx1.java

--- ThreadEx1.java
+++ ThreadEx1.java
@@ -1,28 +1,28 @@
 class A implements Runnable
 {
-public void run()
-{
-int i;
-for(i=0;i<=4;i++)
-System.out.println("Thread A : "+ i);
-}
+  public void run()
+  {
+    int i;
+    for(i=0;i<=4;i++)
+      System.out.println("Thread A : "+ i);
+  }
 }
 class B implements Runnable
 {
-public void run()
-{
-int i;
-for(i=0;i<=4;i++)
-System.out.println("Thread B : "+ i);
-}
+  public void run()
+  {
+  int i;
+  for(i=0;i<=4;i++)
+    System.out.println("Thread B : "+ i);
+  }
 }
 public class ThreadEx1
 {
-public static void main(String args [])
-{
-Thread t1 = new Thread(new A());
-Thread t2 = new Thread(new B()); 
-t1.start();
-t2.start();
-}
+  public static void main(String args [])
+  {
+    Thread t1 = new Thread(new A());
+    Thread t2 = new Thread(new B()); 
+    t1.start();
+    t2.start();
+  }
 }
