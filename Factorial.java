598bf47fed08186667de1d76148aeeb53398f14c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-26 17:27:16 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,14 +1,15 @@
 import java.util.*;
 class Factorial 
 {
-public static void main(String args[] ) throws Exception {
-Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
-int pr=1;
-for( int i=1; i<=n; i++)
-{
-pr = pr * i;
-}
-System.out.println(pr);
-}
+  public static void main(String args[] ) throws Exception 
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int pr=1;
+    for( int i=1; i<=n; i++)
+    {
+      pr = pr * i;
+    }
+    System.out.println(pr);
+  }
 }
