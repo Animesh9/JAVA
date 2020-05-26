aa3eccb7afc98281116c27dd7d07ec753d0c53c5
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-26 17:29:59 +0530

    Update NameLocation.java

--- NameLocation.java
+++ NameLocation.java
@@ -1,31 +1,31 @@
 class data
 {
-String name;
-data(String n){ name=n; }
-void disp()
-{
-System.out.println("_______________________________");
-System.out.println("Name :"+name);
-int c=0;
-int len=name.length();
-for(int i=0;i<len;i++)
-if(name.charAt(i)=='A'||name.charAt(i)=='a')
-{
-c++;
-System.out.println("number of occurance :"+c);
-System.out.println("Possition :"+(i+1));
-}
-if(c==0)
-System.out.println("there is no 'A' available in the string");
-}
+  String name;
+  data(String n){ name=n; }
+  void disp()
+  {
+    System.out.println("_______________________________");
+    System.out.println("Name :"+name);
+    int c=0;
+    int len=name.length();
+    for(int i=0;i<len;i++)
+    if(name.charAt(i)=='A'||name.charAt(i)=='a')
+  {
+  c++;
+  System.out.println("number of occurance :"+c);
+  System.out.println("Possition :"+(i+1));
+  }
+  if(c==0)
+    System.out.println("there is no 'A' available in the string");
+  }
 }
 class NameLocation
 {
-public static void main(String ar[])
-{
-data d1=new data("Animesh Jain");
-d1.disp();
-data d2=new data("Samkit Jain");
-d2.disp();
-}
+  public static void main(String ar[])
+  {
+    data d1=new data("Animesh Jain");
+    d1.disp();
+    data d2=new data("Samkit Jain");
+    d2.disp();
+  }
 }
