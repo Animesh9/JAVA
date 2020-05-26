4d67c1fe6fb04e2c2d7b1fecbf226facce83f1ca
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-26 17:28:52 +0530

    Update This.java

--- This.java
+++ This.java
@@ -1,16 +1,16 @@
 class This 
-{
-int a=552, b=998 ;
-void setdata()
-{
-int c = 0, d = 0;
-c = this.a ;
-d = this.b ;
-System.out.println(c + " " + d);
-}
-public static void main(String args[])
-{
-This obj1 = new This();
-obj1.setdata();
-}
+ {
+  int a=552, b=998 ;
+  void setdata()
+  {
+    int c = 0, d = 0;
+    c = this.a ;
+    d = this.b ;
+    System.out.println(c + " " + d);
+  }
+  public static void main(String args[])
+  {
+    This obj1 = new This();
+    obj1.setdata();
+  }
 }
