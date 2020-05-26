a88da417fd3f5dd2b9946111613166ac25a55439
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-27 02:55:12 +0530

    Update ThreadEx2.java

--- ThreadEx2.java
+++ ThreadEx2.java
@@ -1,28 +1,28 @@
 class A extends Thread
 {
-public void run()
-{
-int i;
-for(i=0;i<=4;i++)
-System.out.println("Thread A : "+ i);
-}
+  public void run()
+  {
+    int i;
+    for(i=0;i<=4;i++)
+      System.out.println("Thread A : "+ i);
+  }
 }
 class B extends Thread
 {
-public void run()
-{
-int i;
-for(i=0;i<=4;i++)
-System.out.println("Thread B : "+ i);
-}
+  public void run()
+  {
+  int i;
+  for(i=0;i<=4;i++)
+    System.out.println("Thread B : "+ i);
+  }
 }
 public class ThreadEx2
 {
-public static void main(String args [])
-{
-A t1 = new A();
-B t2 = new B(); 
-t1.start();
-t2.start();
-}
+  public static void main(String args [])
+  {
+    A t1 = new A();
+    B t2 = new B(); 
+    t1.start();
+    t2.start();
+  }
 }
