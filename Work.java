23db0ad50929d168e214a17d527ef9483c1aac60
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-29 02:20:49 +0530

    Update Work.java

--- Work.java
+++ Work.java
@@ -1,29 +1,29 @@
 class worker
 {
-String name="bajaj";
-int s_rate= 150;
+  String name="bajaj";
+  int s_rate= 150;
 }
 class daily_worker extends worker
 {
-void compay(int hours)
-{
-System.out.println(s_rate*hours);
-}
+  void compay(int hours)
+  {
+    System.out.println(s_rate*hours);
+  }
 }
 class salaried_worker extends worker
-{
-void compay(int hours)
-{
-System.out.println(s_rate*40);
-}
+  {
+    void compay(int hours)
+  {
+  System.out.println(s_rate*40);
+  }
 }
 class Work
 {
-public static void main(String args[])
-{
-daily_worker k= new daily_worker();
-k.compay(48);
-salaried_worker j=new salaried_worker();
-j.compay(48);
-}
+  public static void main(String args[])
+  {
+    daily_worker k= new daily_worker();
+    k.compay(48);
+    salaried_worker j=new salaried_worker();
+    j.compay(48);
+  }
 }
