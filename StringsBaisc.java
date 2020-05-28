8900363a3456c736d049c90ef782762a2d33ac51
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-29 02:19:49 +0530

    Update StringsBaisc.java

--- StringsBaisc.java
+++ StringsBaisc.java
@@ -1,21 +1,24 @@
 import java.io.*;
 import java.util.*;
-public class StringsBasic {
-public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-String A=sc.next();
-String B=sc.next();
-System.out.println(A.length()+B.length());
-int i = A.compareTo(B);
-if(i>0){
-System.out.println("Yes");
-} 
-else
+public class StringsBasic 
 {
-System.out.println("No");
-}
-String atemp = A.substring(0,1).toUpperCase()+A.substring(1);
-String btemp = B.substring(0,1).toUpperCase()+B.substring(1);;
-System.out.println(atemp+" "+btemp);
-}
+  public static void main(String[] args) 
+  {
+    Scanner sc=new Scanner(System.in);
+    String A=sc.next();
+    String B=sc.next();
+    System.out.println(A.length()+B.length());
+    int i = A.compareTo(B);
+    if(i>0)
+    {
+      System.out.println("Yes");
+    } 
+    else
+    {
+      System.out.println("No");
+    }
+    String atemp = A.substring(0,1).toUpperCase()+A.substring(1);
+    String btemp = B.substring(0,1).toUpperCase()+B.substring(1);;
+    System.out.println(atemp+" "+btemp);
+  }
 }
