066507f6137b91c409f95b887cf20d196b7a60f5
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-30 02:57:38 +0530

    Update StringConcat.java

--- StringConcat.java
+++ StringConcat.java
@@ -1,10 +1,10 @@
 public class StringConcat
 {
-public static void main (String[]args)
-{
-  String str1 = "This ";
-  str1 = str1.concat("is ");
-  str1 = str1.concat("a ").concat("Concatenation ").concat("Program.");
-  System.out.println(str1);. // This will show the concatenated string
-}
+    public static void main (String[]args)
+    {
+      String str1 = "This ";
+      str1 = str1.concat("is ");
+      str1 = str1.concat("a ").concat("Concatenation ").concat("Program.");
+      System.out.println(str1);. // This will show the concatenated string
+    }
 }
