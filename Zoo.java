59bee4931c84118ac0037c0ef70dcd644a239ee6
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-30 02:56:43 +0530

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -20,63 +20,63 @@ Implement a menu-driven main program which uses the Zoo and the various animal c
 import java.util.Scanner;
 abstract class Animals
 {
-String name; 
-int age;
-String type;
-Animals(String name , int age , String type)
-{
-this.name = name;
-this.age = age;
-this.type  = type;
-}
-String getName(String name)
-{
-return name;
-}
-int getAge(int age)
-{
-return age;
-}
-void talk()
-{
-System.out.println("Name " + name + "\nAge " + age + "\nType " + type);
-}
+        String name; 
+        int age;
+        String type;
+        Animals(String name , int age , String type)
+        {
+                this.name = name;
+                this.age = age;
+                this.type  = type;
+        }
+        String getName(String name)
+        {
+                return name;
+        }
+        int getAge(int age)
+        {
+                return age;
+        }
+        void talk()
+        {
+                System.out.println("Name " + name + "\nAge " + age + "\nType " + type);
+        }
 }
 class Lion extends Animals
 {
-Lion(String name , int age , String type)
-{
-super(name,age,type);
-super.talk();
-}
+        Lion(String name , int age , String type)
+        {
+                super(name,age,type);
+                super.talk();
+        }
 }
 class Tiger extends Animals
 {
-Tiger(String name,int age,String type)
-{
-super(name,age,type);
-super.talk();
-}
+        Tiger(String name,int age,String type)
+        {
+                super(name,age,type);
+                super.talk();
+        }
 }
 class Zoo 
 {
-public static void main(String args [])
-{
-Scanner s = new Scanner(System.in);
-char input;
-Lion l1 = new Lion("Lion" , 4 ,"Lion");
-Tiger t1 = new Tiger ("Tiger" , 4 ,"Omnivores");
-do
-{
-String name = s.nextLine();
-int age = s.nextInt();
-String type = s.nextLine();
-Lion l2 = new Lion( name ,age  , type );
-System.out.println("Add Animal ") ;
-input = s.next().charAt(0);
-}while (input == 'y');
+        public static void main(String args [])
+        {
+                Scanner s = new Scanner(System.in);
+                char input;
+                Lion l1 = new Lion("Lion" , 4 ,"Lion");
+                Tiger t1 = new Tiger ("Tiger" , 4 ,"Omnivores");
+                do
+                {
+                        String name = s.nextLine();
+                        int age = s.nextInt();
+                        String type = s.nextLine();
+                        Lion l2 = new Lion( name ,age  , type );
+                        System.out.println("Add Animal ") ;
+                        input = s.next().charAt(0);
+                }while (input == 'y');
 
-}
+        }
 }
 //This Question is Incomplete feel free to change ....
 
