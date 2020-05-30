da5f0ba2c1d1f597e395a0499d9b68ba0b1ccc15
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-05-31 00:09:07 +0530

    Update SearchChar.java

--- SearchChar.java
+++ SearchChar.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.util.Scanner;  
 class SearchChar
 { 
-public static void main (String[] args) 
-{
-Scanner s = new Scanner(System.in);
-String str = s.nextLine();
-char a = s.next().charAt(0);
-int firstIndex = str.indexOf(a); 
-System.out.println("First occurrence of char 'a'" + " is found at : " + firstIndex); 
-char b = s.next().charAt(0);
-int lastIndex = str.lastIndexOf(b); 
-System.out.println("Last occurrence of char 'a' is" + " found at : " + lastIndex);
-}
+    public static void main (String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        char a = s.next().charAt(0);
+        int firstIndex = str.indexOf(a); 
+        System.out.println("First occurrence of char 'a'" + " is found at : " + firstIndex); 
+        char b = s.next().charAt(0);
+        int lastIndex = str.lastIndexOf(b); 
+        System.out.println("Last occurrence of char 'a' is" + " found at : " + lastIndex);
+    }
 }
