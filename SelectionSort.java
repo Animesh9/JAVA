68e6a787ecaac29fd177354308b93bcbafe8fbf7
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-01 11:03:10 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,34 +1,33 @@
 import java.util.Scanner;
 public class SelectionSort
 {
-public static void main (String args[])
-{
-int n, k;
-int t=0;
-Scanner s = new Scanner(System.in);
-System.out.println("Enter the Number of elements in array");
-n = s.nextInt();
-int [] i = new int[n];
-for (k=0;k<n;k++)
-{
-i[k]=s.nextInt();
-}
-for (k=0;k<n-1;k++)
-{
-for (int j = k + 1 ; j < n ; j++)
-{
-if(i[k]>i[j])
-{
-t=i[k];
-i[k]=i[j];
-i[j]=t;
-}
-}
-}
-for (k=0;k<n;k++)
-{
-System.out.println(i[k]+" ,");
-}
-
-}
+  public static void main (String args[])
+  {
+    int n, k;
+    int t=0;
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter the Number of elements in array");
+    n = s.nextInt();
+    int [] i = new int[n];
+    for (k=0;k<n;k++)
+    {
+      i[k]=s.nextInt();
+    }
+    for (k=0;k<n-1;k++)
+    {
+      for (int j = k + 1 ; j < n ; j++)
+      {
+        if(i[k]>i[j])
+        {
+          t=i[k];
+          i[k]=i[j];
+          i[j]=t;
+        }
+      }
+    }
+    for (k=0;k<n;k++)
+    {
+      System.out.println(i[k]+" ,");
+    }
+  }
 }
