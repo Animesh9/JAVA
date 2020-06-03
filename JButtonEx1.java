8ffa5c9912cc50a424618ef382422d44f3b59130
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-03 19:43:24 +0530

    Update JButtonEx1.java

--- JButtonEx1.java
+++ JButtonEx1.java
@@ -1,14 +1,14 @@
 import javax.swing.*;
 public class JButtonEx1
 {
-public static void main(String args[])
-{
-JFrame f=new JFrame("JButton Ex");  
-JButton b=new JButton("Click Here");  
-b.setBounds(50,100,95,30);  
-f.add(b);  
-f.setSize(400,400);  
-f.setLayout(null);  
-f.setVisible(true);
-}
+    public static void main(String args[])
+    {
+        JFrame f=new JFrame("JButton Ex");  
+        JButton b=new JButton("Click Here");  
+        b.setBounds(50,100,95,30);  
+        f.add(b);  
+        f.setSize(400,400);  
+        f.setLayout(null);  
+        f.setVisible(true);
+    }
 }
