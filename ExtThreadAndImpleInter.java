c6987e9e0403a4c5f8b21b9a4f40a5ade06e3ce7
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-03 19:44:34 +0530

    Update ExtThreadAndImpleInter.java

--- ExtThreadAndImpleInter.java
+++ ExtThreadAndImpleInter.java
@@ -1,9 +1,9 @@
 class test
 {
-public static void t1()
-{
-System.out.println("In test class ");
-}
+  public static void t1()
+  {
+    System.out.println("In test class ");
+  }
 }
 class ExtThreadAndImpleInter extends test implements Runnable
 {
