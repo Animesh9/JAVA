59263eae08601f5fcc4d5c4cb0a1484f79aebca5
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-07 12:52:46 +0530

    Update IteratorEx.java

--- IteratorEx.java
+++ IteratorEx.java
@@ -1,21 +1,21 @@
 import java.util.*;
 public class IteratorEx
 {
-public static void main(String args[]) 
-{
-Set<String> newSet = new HashSet<String>();
-newSet.add("1");
-newSet.add("2");
-newSet.add("3");
-Iterator<String> itr = newSet.iterator();
-while(itr.hasNext()) 
-{
-System.out.println(itr.next());
-}
-itr.remove();
-for(String var:newSet) 
-{
-System.out.println(var);
-}
-}
+    public static void main(String args[]) 
+    {
+        Set<String> newSet = new HashSet<String>();
+        newSet.add("1");
+        newSet.add("2");
+        newSet.add("3");
+        Iterator<String> itr = newSet.iterator();
+        while(itr.hasNext()) 
+        {
+            System.out.println(itr.next());
+        }
+        itr.remove();
+        for(String var:newSet) 
+        {
+            System.out.println(var);
+        }
+    }
 }
