2a30058c3b0a6162b634c07489ed2927e8043005
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-12 01:44:12 +0530

    Update NumberToMonth.java

--- NumberToMonth.java
+++ NumberToMonth.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-public class NumberToMonth{
-public static void main(String args[])
+public class NumberToMonth
 {
+    public static void main(String args[])
+    {
 Scanner s = new Scanner(System.in);
 System.out.println("Enter the number t find the month " );
 int n = s.nextInt();
