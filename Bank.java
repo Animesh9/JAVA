20ae41498ac2e0f68630dbf9705d068fa715f200
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-14 02:03:09 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,19 +1,19 @@
 //Assignment Q4
 /*
-Assume that a bank maintains two kinds of accounts for customers, one called as savings
-account and other as current account. The savings account provides compound interest and
-withdrawal facilities but no cheque book facility. The current account provides cheque 
-book facility but no interest. Current account holders also maintain a minimum balance 
-and if the balance falls below this level, a service charge is imposed.  Create a class
-account that stores customer name, account number (Should generate automatically if new 
-customer added) and type of account. From this derive the classes cur_acct and sav_acct 
-to make them more specific to their requirements. Include necessary member functions in 
-order to achieve the following tasks: 
-a. Accept deposit from a customer and update the balance 
-b. Display the balance. 
-c. Compute and deposit interest. 
-d. Permit withdrawal andupdate the balance. 
-e. Check for minimum balance, impose penalty, necessary and update the balance.  
+    Assume that a bank maintains two kinds of accounts for customers, one called as savings
+    account and other as current account. The savings account provides compound interest and
+    withdrawal facilities but no cheque book facility. The current account provides cheque 
+    book facility but no interest. Current account holders also maintain a minimum balance 
+    and if the balance falls below this level, a service charge is imposed.  Create a class
+    account that stores customer name, account number (Should generate automatically if new 
+    customer added) and type of account. From this derive the classes cur_acct and sav_acct 
+    to make them more specific to their requirements. Include necessary member functions in 
+    order to achieve the following tasks: 
+    a. Accept deposit from a customer and update the balance 
+    b. Display the balance. 
+    c. Compute and deposit interest. 
+    d. Permit withdrawal andupdate the balance. 
+    e. Check for minimum balance, impose penalty, necessary and update the balance.  
  */
 import java.util.Scanner;
 import java.lang.Math;
