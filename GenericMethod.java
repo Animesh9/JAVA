b61ec2634f790de0e7460a0a4636b3eceb38dc2d
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-16 01:44:39 +0530

    Update GenericMethod.java

--- GenericMethod.java
+++ GenericMethod.java
@@ -1,12 +1,12 @@
 public class GenericMethod
 {
-public <A> void printArray(A []s)
-{
-for (A x:s)
-System.out.println(x);
-}
-public static void main(String args[])
-{
+    public <A> void printArray(A []s)
+    {
+        for (A x:s)
+            System.out.println(x);
+    }
+    public static void main(String args[])
+    {
 GenericMethod gm1 = new GenericMethod();
 String people[] = new String[]{"Ani","Animesh"};
 Integer number[]={9,16,45};
