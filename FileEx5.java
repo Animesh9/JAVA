cec0260cdf503dc036dbaf2b7f6a6526a63c8e6e
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-17 03:06:11 +0530

    Update FileEx5.java

--- FileEx5.java
+++ FileEx5.java
@@ -1,11 +1,12 @@
 import java.io.*;
-public class FileEx5{
-public static void main(String args []) throws IOException
+public class FileEx5
 {
-int ch ;
-BufferedReader bf = new BufferedReader(new FileReader("d:/java/file3.txt"));
-char []s = new char[20];
-bf.read(s,2	,15);
-bf.close();
-}
+    public static void main(String args []) throws IOException
+    {
+          int ch ;
+          BufferedReader bf = new BufferedReader(new FileReader("d:/java/file3.txt"));
+          char []s = new char[20];
+          bf.read(s,2	,15);
+          bf.close();
+    }
 }
