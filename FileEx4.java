f28bc8bb256ef32d551c61e883431b30647b83dd
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-18 02:47:54 +0530

    Update FileEx4.java

--- FileEx4.java
+++ FileEx4.java
@@ -1,11 +1,11 @@
 import java.io.*;
 public class FileEx4
 {
-public static void main(String [] args) throws IOException
-{
-FileWriter fw = new FileWriter("d:/java/file3.txt",true);
-BufferedWriter bw = new BufferedWriter(fw);
-bw.write("New Day New Way");
-bw.close();
-}
+    public static void main(String [] args) throws IOException
+    {
+        FileWriter fw = new FileWriter("d:/java/file3.txt",true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("New Day New Way");
+        bw.close();
+    }
 }
