bc7c98e0e58b740a1cb993c1941f05575bbb210f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-19 03:49:01 +0530

    Update FileEx3.java

--- FileEx3.java
+++ FileEx3.java
@@ -1,16 +1,17 @@
 import java.io.*;
 public class FileEx3
 {
-public static void main(String [] args) throws IOException
-{
-int i;
-FileOutputStream fout = new FileOutputStream("d:/java/file2.txt",true);
-FileInputStream fin = new FileInputStream("d:/java/file2.txt");
-String s= "new day new way";
-char ch[] = s.toCharArray();
-for(i=0 ; i<s.length() ; i++)
-fout.write(ch[i]);
-fout.close();
+    public static void main(String [] args) throws IOException
+    {
+        int i;
+        FileOutputStream fout = new FileOutputStream("d:/java/file2.txt",true);
+        FileInputStream fin = new FileInputStream("d:/java/file2.txt");
+        String s= "new day new way";
+        char ch[] = s.toCharArray();
+        for(i=0 ; i<s.length() ; i++)
+            fout.write(ch[i]);
+
+        fout.close();
 do 
 {
 i = fin.read();
