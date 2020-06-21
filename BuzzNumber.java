f18a0583857f2aa7a5b5a4e5191ae93b1fb4e5c7
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-21 14:55:32 +0530

    Update BuzzNumber.java

--- BuzzNumber.java
+++ BuzzNumber.java
@@ -3,6 +3,9 @@ public class BuzzNumber
 {
     public static void main(String args[])
     {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number to check if number is buzz number or not.");
+        int a = s.nextInt();
         
     }
 }
