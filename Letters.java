1414cc91d98b3d11a6005cc9bc11a91e93efde92
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-22 02:08:03 +0530

    Update Letters.java

--- Letters.java
+++ Letters.java
@@ -1,17 +1,17 @@
-class Letters{ 
-static void convertOpposite(StringBuffer str) 
+class Letters
 { 
-int ln = str.length(); 
-for (int i=0; i<ln; i++) 
-{ 
-Character c = str.charAt(i); 
-if (Character.isLowerCase(c)) 
-str.replace(i, i+1, Character.toUpperCase(c)+""); 
-else
-str.replace(i, i+1, Character.toLowerCase(c)+""); 
-} 
-} 
-
+    static void convertOpposite(StringBuffer str) 
+    { 
+        int ln = str.length(); 
+        for (int i=0; i<ln; i++) 
+        { 
+            Character c = str.charAt(i); 
+            if (Character.isLowerCase(c)) 
+                str.replace(i, i+1, Character.toUpperCase(c)+""); 
+            else
+                str.replace(i, i+1, Character.toLowerCase(c)+""); 
+        } 
+    } 
 public static void main(String[] args) 
 { 
 StringBuffer str = new StringBuffer("hey how are you"); 
