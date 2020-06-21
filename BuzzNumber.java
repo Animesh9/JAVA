a31b567e41960aa8ce36c238043269fbb4cf6774
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-21 14:55:43 +0530

    Update BuzzNumber.java

--- BuzzNumber.java
+++ BuzzNumber.java
@@ -6,6 +6,13 @@ public class BuzzNumber
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a number to check if number is buzz number or not.");
         int a = s.nextInt();
-        
+        if(a/7 = 0 || a/10 =7)
+        {
+            System.out.println(a + "is buzz number");
+        }
+        else
+        {
+            System.out.println(a + "is not a buzz number");
+        }
     }
 }
