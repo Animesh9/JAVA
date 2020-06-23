ddcb18b6080025f7771ee94a427827e55c728d6e
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-24 01:29:54 +0530

    Update OddEvenInThreads.java

--- OddEvenInThreads.java
+++ OddEvenInThreads.java
@@ -1,23 +1,25 @@
 class even extends Thread
 {
-Thread t=null;
-even()
-{
-t=new Thread(this);
-start();
-}
-public void run()
-{
-try
-{
-for(int i=2;i<50;i+=2)
-System.out.print(i+" ");
-Thread.sleep(100);
-}
-catch(Exception e)
-{System.out.println("thread interepted");
-}
-}
+    Thread t=null;
+    even()
+    {
+        t=new Thread(this);
+        start();
+    }
+    public void run()
+    {
+        try
+        {
+            for(int i=2;i<50;i+=2)
+                System.out.print(i+" ");
+            
+            Thread.sleep(100);
+        }
+        catch(Exception e)
+        {
+            System.out.println("thread interepted");
+        }
+    }
 }
 class odd extends Thread
 {
