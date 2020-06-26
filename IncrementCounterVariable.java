5120209c01f319828b04727ddd486bb24036d438
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-27 02:40:30 +0530

    Update IncrementCounterVariable.java

--- IncrementCounterVariable.java
+++ IncrementCounterVariable.java
@@ -1,19 +1,19 @@
 class IncrementCounterVariable
 {
-public static void main(String arg[])throws Exception
-{
-data d1=new data();
-data d2=new data();
-data d3=new data();
-data d4=new data();
-data d5=new data();
-data d6=new data();
-data d7=new data();
-data d8=new data();
-data d9=new data();
-data d10=new data();
-System.out.println(d10.count);
-}
+    public static void main(String arg[])throws Exception
+    {
+        data d1=new data();
+        data d2=new data();
+        data d3=new data();
+        data d4=new data();
+        data d5=new data();
+        data d6=new data();
+        data d7=new data();
+        data d8=new data();
+        data d9=new data();
+        data d10=new data();
+        System.out.println(d10.count);
+    }
 }
 class item { static int count=0; }
 class data extends item implements Runnable
