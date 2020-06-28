ec22881ca3509f1e1700b475eae55158d9fbe444
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-06-29 03:51:22 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -17,18 +17,18 @@ public class Calc
             sub=a-b;      //Subtraction 
             mul=a*b;      //Multiply
             div=a/b;      //Division
-if(n==1)
-System.out.println("Addition is "+ add );
-else if(n==2)
-System.out.println("Subtraction is "+ sub );
-else if(n==3)
-System.out.println("Multiplication is "+ mul );
-else if(n==4)
-System.out.println("Division is "+ div );
-else
-System.out.println("Wrong call please try again!!!" );
-System.out.println("if u want to try again press y" );
-dec = Calc.next().charAt(0);
-}while(dec=='y');//if decisision is y = yes then the loop will recurse again
-}
+            if(n==1)
+            System.out.println("Addition is "+ add );
+            else if(n==2)
+            System.out.println("Subtraction is "+ sub );
+            else if(n==3)
+            System.out.println("Multiplication is "+ mul );
+            else if(n==4)
+            System.out.println("Division is "+ div );
+            else
+            System.out.println("Wrong call please try again!!!" );
+            System.out.println("if u want to try again press y" );
+            dec = Calc.next().charAt(0);
+            }while(dec=='y');//if decisision is y = yes then the loop will recurse again
+    }
 }
