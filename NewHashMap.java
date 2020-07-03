36562e7990e6b806c1e8faaa1c5f4ab07802d55f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-07-03 13:11:40 +0530

    Update NewHashMap.java

--- NewHashMap.java
+++ NewHashMap.java
@@ -3,15 +3,15 @@ import java.util.Iterator;
 import java.util.Map;
 class NewHashMap
 {
-public static void main(String args[])
-{
-HashMap<String, Integer> map = new HashMap<>(); 
-map.put("Animesh", 10); 
-map.put("Ani", 30); 
-map.put("Roman", 20);
-map.put("Kush", 20);
-System.out.println();
-String key = "Animesh";
+    public static void main(String args[])
+    {
+        HashMap<String, Integer> map = new HashMap<>(); 
+        map.put("Animesh", 10); 
+        map.put("Ani", 30); 
+        map.put("Roman", 20);
+        map.put("Kush", 20);
+        System.out.println();
+        String key = "Animesh";
 if (map.containsKey(key)) 
 {
 Integer a = map.get(key); 
