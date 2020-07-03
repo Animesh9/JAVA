499065a11f336b73400de3435240293658f2c23c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-07-03 13:12:58 +0530

    Update NewHashMap.java

--- NewHashMap.java
+++ NewHashMap.java
@@ -12,24 +12,24 @@ class NewHashMap
         map.put("Kush", 20);
         System.out.println();
         String key = "Animesh";
-if (map.containsKey(key)) 
-{
-Integer a = map.get(key); 
-System.out.println("value for key "+ key  +" is: "+a); 
-}
-int value = 20; 
-System.out.println();
-if (map.containsValue(value)) 
-{
-Iterator it = map.entrySet().iterator();
-while(it.hasNext()){ 
-String str = it.next().toString();
-if(str.split("=")[1].contains(String.valueOf(value)))
-{
-System.out.println("key for value "+ value  +" is: "+str.split("=")[0]); 
-}
-}
-} 
+        if (map.containsKey(key)) 
+        {
+            Integer a = map.get(key); 
+            System.out.println("value for key "+ key  +" is: "+a); 
+        }
+        int value = 20; 
+        System.out.println();
+        if (map.containsValue(value)) 
+        {
+            Iterator it = map.entrySet().iterator();
+            while(it.hasNext()){ 
+            String str = it.next().toString();
+            if(str.split("=")[1].contains(String.valueOf(value)))
+            {
+                System.out.println("key for value "+ value  +" is: "+str.split("=")[0]); 
+            }
+        }
+    } 
 System.out.println();
 System.out.println("Printing as key set :");
 Iterator it = map.keySet().iterator();
