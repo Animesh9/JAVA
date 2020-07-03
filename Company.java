3e778d5004c0fa10737bfc8590ad5d486d878c75
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-07-04 02:44:24 +0530

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,15 +1,15 @@
 import java.util.*;
 class employee
 {
-String name;
-int empid;
-int salary;
-employee()
-{
-name="Animesh";
-empid=22;
-salary=9000;
-}
+    String name;
+    int empid;
+    int salary;
+    employee()
+    {
+        name="Animesh";
+        empid=22;
+        salary=9000;
+    }
 employee(String n,int e,int s)
 {
 name=n;
