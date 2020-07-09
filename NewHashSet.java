4e792a3cf0dba59d6163b4fa0cadb40b2a43cb1c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-07-10 00:07:19 +0530

    Update NewHashSet.java

--- NewHashSet.java
+++ NewHashSet.java
@@ -5,17 +5,17 @@ class NewHashSet
 {
     public static void main(String agr[])
     {
-List names = new ArrayList<String>();
-names.add("Animesh");
-names.add("Ani");
-names.add("Kush");
-names.add("Roman");
-names.add("Chirag");
-HashSet hNames = new HashSet<>(names);
-System.out.println("Initial list :"+hNames);  
-String key = "Ani";
-System.out.println("Contains key '" + key+ "' ? :" + hNames.contains(key));
-hNames.clear();
-System.out.println("After clear() list :"+hNames);  
-}
+        List names = new ArrayList<String>();
+        names.add("Animesh");
+        names.add("Ani");
+        names.add("Kush");
+        names.add("Roman");
+        names.add("Chirag");
+        HashSet hNames = new HashSet<>(names);
+        System.out.println("Initial list :"+hNames);  
+        String key = "Ani";
+        System.out.println("Contains key '" + key+ "' ? :" + hNames.contains(key));
+        hNames.clear();
+        System.out.println("After clear() list :"+hNames);  
+    }
 }
