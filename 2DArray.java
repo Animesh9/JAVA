5f62f23f9255d5b887a2442e59e7c328bbdde690
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-07-19 21:29:12 +0530

    Update 2DArray.java

--- 2DArray.java
+++ 2DArray.java
@@ -4,6 +4,13 @@ public class Solution
 {
     public static void main(String[] args)
     {
-
+        int a[][] = new int[6][6];
+        int maxSum = Integer.MIN_VALUE;
+        try (Scanner scanner = new Scanner(System.in);)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
     }
 }
