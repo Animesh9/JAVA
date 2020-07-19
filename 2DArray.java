b225dc3aee2d85c256d2557a19b2ddb84f69d6a5
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-07-19 21:29:35 +0530

    Update 2DArray.java

--- 2DArray.java
+++ 2DArray.java
@@ -12,5 +12,22 @@ public class Solution
             {
                 for(int j = 0; j < 6; j++)
                 {
+                    a[i][j] = scanner.nextInt();
+                    if (i > 1 && j > 1)
+                    {
+                        int sum =
+                            a[i][j]
+                            + a[i][j-1]
+                            + a[i][j-2]
+                            + a[i-1][j-1]
+                            + a[i-2][j]
+                            + a[i-2][j-1]
+                            + a[i-2][j-2];
+                        if (sum > maxSum) {maxSum = sum;}
+                    }
+                }
+            }
+        }
+
     }
 }
