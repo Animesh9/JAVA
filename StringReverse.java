b5c5211369cf082d965ea2bc161adfbd07f11383
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-07-25 12:36:22 +0530

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -8,6 +8,9 @@ public class Solution {
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-        
+        int count = 0;
+        for(int i=0; i<A.length()/2;i++)
+            if( A.charAt(i) == A.charAt(A.length()-i-1)) count++;
+        System.out.println( (count == A.length()/2) ? "Yes" : "No") ;
     }
 }
